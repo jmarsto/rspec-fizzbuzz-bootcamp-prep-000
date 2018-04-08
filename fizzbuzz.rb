df33b15269cf117272a74354f0cc9bc43e4cd74b
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-04-08 16:11:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,10 @@
 def fizzbuzz(int)
   if (int/3) === (int/3).round
     "Fizz"
+  end
   elsif (int/5) === (int/5).round
     "Buzz"
+  end
     elsif (int/3) === (int/3).round && int/5 === (int/5).round
     "FizzBuzz"
   end
